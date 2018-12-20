e93f434f55ac62c5ba0c6b52ead032ce45bba738
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-12-20 04:43:04 +0200

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -4,42 +4,38 @@
 
 template <typename T, typename Less = std::less<T>>
 class AVL{
-
   public:
-
     AVL() { root_ = nullptr; }
 
     void insert(const T& val) { insert(val, root_); }
-
-    //void remove(const T& val) { remove(val, root_); }
 
     void postorder() { postorder(root_); }
 
     bool operator[](const T& val) {
       node* curr = root_;
-      while (curr != nullptr){
+      while (curr != nullptr) {
         if (curr->data == val) return true;
-        bool it = less_(val,curr->data);
+        bool it = less_(val, curr->data);
         curr = curr->next[it ? 0 : 1];
       }
       return false;
     }
 
     const T& operator()(size_t k) {
-      if (k <= 0 || k > root_->count){
-        throw "Out of Range error: \n";
+      if (k <= 0 || k > root_->count) {
+        throw "Out of Range error: \n";  // fix me
       }
       node* nnode = root_;
-      do{
+      do {
         size_t size = 0;
         if (root_->next[0] != nullptr) {
           size = root_->next[0]->count;
         }
         nnode = root_;
-        bool it = less_(k,size+1);
-        root_ = root_->next[it ? 0 : 1];
-        if (it == 0) k -= size+1;
-      } while (k>=1);
+        bool pos = less_(k,size + 1);
+        root_ = root_->next[pos ? 0 : 1];
+        if (!pos) k -= size + 1;
+      } while (k >= 1);
       return nnode->data;
     }
 
@@ -66,17 +62,19 @@ class AVL{
         root = new node(val);
         return;
       }
-      bool it = less_(val,root->data);
-      int bf = it ? -1 : 1;
-      insert(val,root->next[it ? 0 : 1]);
+      bool pos = less_(val, root->data);
+      int bf = pos ? -1 : 1;
+      insert(val,root->next[pos ? 0 : 1]);
       root->count++;
       root->balance+=bf;
       rotate(val,root);
-      root->balance=0;
-      if(root->next[0] != nullptr)
-        root->next[0]->balance=0;
-      if(root->next[1] != nullptr)
-        root->next[1]->balance=0;
+      root->balance = 0;
+      if(root->next[0] != nullptr) {
+        root->next[0]->balance = 0;
+      }
+      if(root->next[1] != nullptr) {
+        root->next[1]->balance = 0;
+      }
     }
 
     node* single_rotation(node* root, bool pos) {
@@ -88,16 +86,18 @@ class AVL{
       return curr;
     }
 
-    void double_rotation(node*& root, bool pos) { root->next[pos] = single_rotation(root->next[pos],pos); }
+    void double_rotation(node*& root, bool pos) {
+      root->next[pos] = single_rotation(root->next[pos], pos);
+    }
 
     void rotate(const T& val, node*& root) {
-      if (root->balance > 1) {
+      if (root->balance > 1) {  // fix me
         if (root->next[0]->balance > 0) {
           root = single_rotation(root,0);
         } else {
           double_rotation(root,0);
         }
-      } else if (root->balance < -1) {
+      } else if (root->balance < -1) {  // fix me
         if (root->next[1]->balance > 0) {
           root = single_rotation(root,1);
         } else {
@@ -106,105 +106,38 @@ class AVL{
       }
     }
 
-    void postorder(node* root) {
-      if (root == nullptr) return;
-      postorder(root->next[0]);
-      postorder(root->next[1]);
-      std::cout << root->data << " ";
-    }
-
     void destroy(node* root) {
       if (root == nullptr) return;
       destroy(root->next[0]);
       destroy(root->next[1]);
       delete(root);
     }
-
-    // void remove(const T& val, node*& root) {
-    //   if (root == nullptr) return;
-    //   if (root->data == val) { del(root); return; }
-    //   bool it = less_(val,root->data);
-    //   remove(val,root->next[it ? 0 : 1]);
-    //   if (root == nullptr) return;
-    //   //rotate(val,root);
-    // }
-    //
-    // void del(node*& root) {
-    //   if (root->next[0] && (root)->next[1]){
-    //       node* temp = find_min((root)->next[1]);
-    //
-    //       //(*root)->data = temp->data;
-    //       node* curr = temp;
-    //       temp = root;
-    //       root = curr;
-    //       //delete(temp);
-    //       remove(temp->data,temp->next[1]);
-    //  } else if (!(root)->next[0]){
-    //     node* temp = root;
-    //     root = (root)->next[1];
-    //     delete temp;
-    //
-    //   } else if (!(root)->next[1]){
-    //     node* temp = root;
-    //     root = (root)->next[0];
-    //     delete temp;
-    //   }
-    // }
-    //
-    // node* find_min(node* root) {
-    //   if (root == nullptr) return root;
-    //   if (root->next[0] == nullptr) return root;
-    //   return (root->next[0]);
-    // }
-
 };
 
-int main(void){
+int main(void) {
   AVL<int> avl;
-  srand(time(NULL));
-  for (int i=1; i<=100; i++){
-      int num = rand()%100+1;
-      if (avl[num] == false)
-        avl.insert(num);
-  }
-  // avl.insert(8);
-  // avl.insert(5);
-  // avl.insert(4);
-  // avl.insert(11);
-  // avl.insert(15);
-  // avl.insert(3);
-  // avl.insert(6);
-  // avl.insert(2);
-  // avl.insert(1);
-  // avl.insert(19);
-  // avl.insert(7);
-  // avl.insert(21);
-  // avl.insert(25);
-  // avl.insert(7);
-  // avl.insert(90);
-  // avl.insert(99);
-  // avl.insert(31);
-  // avl.insert(5);
-  // avl.insert(54);
-  // avl.insert(64);
-  // avl.insert(27);
-  // avl.insert(82);
-  // avl.insert(29);
-  // avl.insert(7);
-  // avl.insert(90);
-  // avl.insert(99);
-  // avl.insert(31);
-//  avl.postorder();
+  int n = 1000000;
 
-  // std::cout << std::endl;
-  //
-  //avl.remove(15);
-  //avl.remove(8);
-  // std::cout << avl[16] << std::endl;
-  // std::cout << avl[4] << std::endl;
-  avl.postorder();
-  std::cout << std::endl;
-  // std::cout << avl(1) << std::endl;
-  // //std::cout << avl(1) << std::endl;
-  // std::cout << std::endl;
+  srand(time(NULL));
+  
+  clock_t c = clock();
+  for (int i = 0; i < n; ++i) {
+    int num = rand();
+    if (!avl[num]) {
+      avl.insert(num);
+    } else {
+      --i;
+    }
+  }
+  c = clock() - c;
+  double s = c * 1.0 / CLOCKS_PER_SEC;
+  std::cout << n << " random inserts in " << s << " sec" << std::endl;
+
+  c = clock();
+  for (int i = 0; i < n; ++i) {
+    avl.insert(i);
+  }
+  c = clock() - c;
+  s = c * 1.0 / CLOCKS_PER_SEC;
+  std::cout << n << " sequential inserts in " << s << " sec" << std::endl;
 }

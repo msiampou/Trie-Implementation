b67263a1549266842c1d5e7c3f50713e52e2af88
Maria Despoina Siampou <siampoumaria@gmail.com>
2019-02-27 23:36:05 +0200

    Update Stack.c

--- stack/Stack.c
+++ stack/Stack.c
@@ -8,44 +8,37 @@
  * and rear on top
  */
 void Push(Stack** head, Stack** rear, char val){
-
-    if(*head==NULL){
-        (*head) = (Stack*)malloc(sizeof(Stack));
-        (*head)->letter = val;
-        (*head)->next = NULL;
-        (*rear) = (*head);
-        (*head)->prev = NULL;
-    }
-    else{
-        Stack *temp = (Stack*)malloc(sizeof(Stack));
-        temp->letter = val;
-        temp->next = NULL;
-
-        (*rear)->next = temp;
-        temp->prev = (*rear);
-        (*rear) = temp;
-    }
+  if(*head==NULL){
+    (*head) = (Stack*)malloc(sizeof(Stack));
+    (*head)->letter = val;
+    (*head)->next = NULL;
+    (*rear) = (*head);
+    (*head)->prev = NULL;
+  }
+  else{
+    Stack *temp = (Stack*)malloc(sizeof(Stack));
+    temp->letter = val;
+    temp->next = NULL;
+    (*rear)->next = temp;
+    temp->prev = *rear;
+    *rear = temp;
+  }
 }
 
 void Pop(Stack** head, Stack** rear){
-
-    if(head!=NULL){
-        Stack* temp = (*rear);
-        if((*rear)==(*head))
-            (*head) = NULL;
-
-        (*rear) = temp->prev;
-        free(temp);
-
-        if((*rear)!=NULL)
-            (*rear)->next=NULL;
-    }
+  if(head!=NULL){
+    Stack* temp = (*rear);
+    if(*rear == *head) (*head) = NULL;
+    *rear = temp->prev;
+    free(temp);
+    if(*rear != NULL) (*rear)->next=NULL;
+  }
 }
 
 void PrintStack(Stack** head){
-    Stack* ptr = (*head);
-    while(ptr!=NULL){
-        printf("%c",ptr->letter);
-        ptr = ptr->next;
-    }
+  Stack* ptr = *head;
+  while(ptr!=NULL){
+    printf("%c",ptr->letter);
+    ptr = ptr->next;
+  }
 }
